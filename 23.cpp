5ba2985a323fc24ccd7a192b42084e9a2f42d3d0
Keisstraje <98694393+Keisstraje@users.noreply.github.com>
2025-11-11 12:31:51 +0300

    Update 23.cpp

--- 23.cpp
+++ 23.cpp
@@ -1,19 +1,33 @@
 #include <iostream>
-#include <iomanip>
-
 using namespace std;
 
 int main() {
-	setlocale(0, "");
-	int arr[5];
-	int i;
-	cout << arr << endl;
-	for (int e = 0; e < 5; e++) {
-		cin >> arr[i];
 
-		cout << arr << endl;
-	}
-	cout << arr <<endl;
+    const int size = 5;
+    setlocale(0, "");
+    int numbers[size];
 
+    cout << "Введите " << size << " целых чисел:\n" << endl;
 
+    for (int i = 0; i < size; ++i) {
+
+      cin >> numbers[i];
+
+    }
+
+    for (int i = 0; i < size; i++) {
+        cout << "| Таблица № "<< i+1<<  endl;
+        cout << "|_________________________________________| " << endl;
+        cout << "|  Число   |      Пример      | Результат |" << endl;
+        cout << "|_________________________________________| " << endl;
+        cout << "|   " << numbers[i] << "      |        " << numbers[i] << "+" << numbers[i] << "       |          " << numbers[i] + numbers[i] << "|" << endl;
+        cout << "|_________________________________________| " << endl;
+        cout << "|   " << numbers[i] << "      |        " << numbers[i] << "-" << numbers[i] << "       |          " << numbers[i] - numbers[i] << "|" << endl;
+        cout << "|_________________________________________| " << endl;
+        cout << "|   " << numbers[i] << "      |        " << numbers[i] << "/" << numbers[i] << "       |          " << numbers[i] / numbers[i] << "|" << endl;
+        cout << "|_________________________________________| " << endl;
+        cout << "|   " << numbers[i] << "      |       " << numbers[i] << "*" << numbers[i] << "       |          " << numbers[i] * numbers[i] << "|" << endl;
+        cout << "|_________________________________________| " << endl;
+
+    }
 }
