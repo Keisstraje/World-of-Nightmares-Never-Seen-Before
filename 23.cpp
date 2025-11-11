f1b1745be1a6424b40b72b806c300105f355f53a
Keisstraje <98694393+Keisstraje@users.noreply.github.com>
2025-11-11 11:41:29 +0300

    Create 23.cpp

---
+++ 23.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+	setlocale(0, "");
+	int arr[5];
+	int i;
+	cout << arr << endl;
+	for (int e = 0; e < 5; e++) {
+		cin >> arr[i];
+
+		cout << arr << endl;
+	}
+	cout << arr <<endl;
+
+
+}
